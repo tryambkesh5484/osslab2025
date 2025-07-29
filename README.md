f637c2d5cc6c7d37306c6c96222cc74f32c94616
231B366 <231B366@juet.co.in>
2025-07-29 15:30:15 +0530

    this is a updated file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # osslab2025
+name TRYAMBKESH 
+roll no 231b366
+email 231b366@juetguna.in
+<Solution code to part F>
---
+++ le1/sum_of_two_no.py
@@ -0,0 +1,19 @@
+
+
+# Function to add two numbers
+def add_two_numbers(a, b):
+    """
+    This function takes two numbers as input and returns their sum.
+    """
+    return a + b
+
+
+num1 = 15
+num2 = 23
+
+
+result = add_two_numbers(num1, num2)
+
+
+print(f"The sum of {num1} and {num2} is: {result}")
+
